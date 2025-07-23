ef704b049317d75ee83035cc98e4da5c4ae4173c
Alex <oleksandr100204@gmail.com>
2025-07-24 01:05:50 +0200

    added section portfolio and set hover effects on the btn and links

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,10 @@ close = document.querySelector(".menu__close");
 //  ACTIVATION AND CLOSE MENU
 burger.addEventListener("click", () => { menu.classList.add("active"); });
 close.addEventListener("click", () => { menu.classList.remove("active"); });
+
+const counters = document.querySelectorAll(".statusbars__item-percent"),
+      lines = document.querySelectorAll(".statusbars__item-progress div");
+
+counters.forEach((item , i) => {
+    lines[i].style.width = item.innerHTML;
+});
