eb42664677acd7ba424475a17aa71adac2a41f28
Alex <oleksandr100204@gmail.com>
2025-07-28 00:16:16 +0200

    added black theme and set menu navigation button link

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,15 @@
 const burger = document.querySelector(".burger"),
 menu = document.querySelector(".menu"),
-close = document.querySelector(".menu__close");
+close = document.querySelector(".menu__close"),
+link = document.querySelectorAll(".menu__link a");
 //  ACTIVATION AND CLOSE MENU
 burger.addEventListener("click", () => { menu.classList.add("active"); });
 close.addEventListener("click", () => { menu.classList.remove("active"); });
+link.forEach((linkItem) => {
+    linkItem.addEventListener("click", () => {
+         menu.classList.remove("active");
+    });
+});
 
 const counters = document.querySelectorAll(".statusbars__item-percent"),
       lines = document.querySelectorAll(".statusbars__item-progress div");
@@ -11,3 +17,16 @@ const counters = document.querySelectorAll(".statusbars__item-percent"),
 counters.forEach((item , i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+const darkModeSwitch = document.querySelector(".input"),
+      body = document.querySelector("body");
+darkModeSwitch.addEventListener("change", () => {
+    if (darkModeSwitch.checked === true) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    }
+    else {
+        body.style.backgroundColor = "white";
+         body.style.color = "black";
+    }
+})
